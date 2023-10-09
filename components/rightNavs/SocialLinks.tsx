1d4a953429a99458b86e86a59713c5cbdcc49466
WooFoo <sujanstha3220@gmail.com>
2023-10-10 01:06:17 +0545

    mt only in big screen

--- components/rightNavs/SocialLinks.tsx
+++ components/rightNavs/SocialLinks.tsx
@@ -2,7 +2,7 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function SocialLinks() {
 	return (
-		<div className="mt-4">
+		<div className="sm:mt-4">
 			<ul className=" p-1 text-2xl h-fit flex gap-2 items-center justify-center">
 				<li className="cursor-pointer  hover:text-slate-600">
 					<a href="https://github.com/darkweb19" target="__blank">
