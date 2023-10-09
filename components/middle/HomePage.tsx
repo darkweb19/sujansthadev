a384748169b177f707437741d1896caf78a3f6af
WooFoo <sujanstha3220@gmail.com>
2023-10-10 01:05:55 +0545

    text size and padding changes

--- components/middle/HomePage.tsx
+++ components/middle/HomePage.tsx
@@ -2,17 +2,17 @@ import Typing from "./Typing";
 
 export default function HomePage() {
 	return (
-		<div className=" flex items-center h-full">
+		<div className=" flex items-center w-full h-full">
 			<div className=" flex flex-col justify-center  items-center h-2/4 ">
-				<div className=" w-3/5 ">
-					<p className="text-5xl text-start font-bold gradient-text ">
+				<div className=" w-4/5 ">
+					<p className="text-3xl sm:text-5xl text-start font-bold gradient-text ">
 						HELLO WORLD
 					</p>
-					<div className="flex  p-1  text-3xl font-semibold text-slate-600">
+					<div className="flex  sm:p-1 text-2xl sm:text-3xl font-semibold text-slate-600">
 						<Typing />
 					</div>
-					<div className=" p-1  w-4/5 text-slate-700">
-						<p className="text-justify font-medium text-lg">
+					<div className=" p-1  w-4/5 text-slate-800">
+						<p className="text-justify font-medium text-base sm:text-lg">
 							I am a Software Engineer and a student from Nepal. I
 							have a keen interest in Full-Stack Development,
 							Artificial Intelligence, IOT and everything in
