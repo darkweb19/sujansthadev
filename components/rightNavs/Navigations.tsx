74343608816f97c793254c7992f68dd339480921
WooFoo <sujanstha3220@gmail.com>
2023-10-10 01:06:41 +0545

    hidden on small sizes only

--- components/rightNavs/Navigations.tsx
+++ components/rightNavs/Navigations.tsx
@@ -1,7 +1,7 @@
 export default function Navigations() {
 	return (
-		<div className=" p-1 mb-3">
-			<ul className="flex flex-col text-gray-500 font-medium ">
+		<div className="hidden sm:p-1 sm:block mb-3">
+			<ul className="flex flex-col text-gray-500 text-xs sm:text-lg font-medium ">
 				<li className="cursor-pointer">/experience</li>
 				<li className="cursor-pointer">/creations</li>
 				<li className="cursor-pointer">/education</li>
