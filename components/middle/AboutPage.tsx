6032ddf46d5fd0ff19e81df657a8abb634ecaaff
WooFoo <sujanstha3220@gmail.com>
2023-10-10 12:40:11 +0545

    myTechStack in list

--- components/middle/AboutPage.tsx
+++ components/middle/AboutPage.tsx
@@ -25,10 +25,24 @@ export default function AboutPage() {
 						Tribhuwan University.
 					</a>{" "}
 				</p>
+				<div className="border px-3 ">
+					<p className="font-medium text-lg">
+						Technologies I have been working with
+					</p>
+					<ul className="text-base font-medium  px-4 list-disc">
+						<li>TypeScript</li>
+						<li>Node Js</li>
+						<li>React Js</li>
+						<li>Next Js</li>
+						<li>GraphQl</li>
+						<li>MongoDb</li>
+						<li>PostgreSQL</li>
+					</ul>
+				</div>
 			</div>
 			<Image
 				src={mine}
-				className="rounded-xl shadow-4xl  sm:block"
+				className="rounded-xl shadow-4xl  sm:block h-auto w-auto"
 				alt="My Pic"
 				height={250}
 				width={250}
