62ff2cda1a812d7966dfe0edf6d352cbf0379966
WooFoo <sujanstha3220@gmail.com>
2023-10-10 20:30:36 +0545

    code refactored by creating a card

---
+++ components/middle/Cards.tsx
@@ -0,0 +1,11 @@
+export default function Cards() {
+	return (
+		<article className="card shadow-5xl w-[250px] h-[280px] sm:w-[400px] sm:h-[350px]">
+			{/* card header  */}
+			<header className="">
+				<h1>Title</h1>
+				<h3>Card trick</h3>
+			</header>
+		</article>
+	);
+}
