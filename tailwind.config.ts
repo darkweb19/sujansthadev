ae64cf37736774ae82b90501cea4e0b7f3ce2575
WooFoo <sujanstha3220@gmail.com>
2023-10-11 13:50:09 +0545

    changed some background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,7 @@ const config: Config = {
 			},
 			boxShadow: {
 				"4xl": "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-				// "5xl": "rgba(0, 0, 0, 0.45) -1rem 25px 20px -20px",
-				"5xl": "rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px",
+				"5xl": "rgba(0, 0, 0, 0.45) -1rem 15px 20px -20px",
 			},
 		},
 	},
