1975365ccce79155ab6cf314fccc2f61488117e5
WooFoo <sujanstha3220@gmail.com>
2023-10-12 12:37:30 +0545

    changed everything to server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-"use client";
 import LandingPage from "@/components/LandingPage";
-import Loadings from "@/components/Loadings";
-import { useEffect, useState } from "react";
+import AllContent from "@/components/middle/AllContent";
 
 export default function Homes() {
-	const [loading, setLoading] = useState(true);
-	useEffect(() => {
-		setLoading(true);
-		setTimeout(() => {
-			setLoading(false);
-		}, 900);
-	}, []);
-
 	return (
 		<div className="">
-			{loading ? <Loadings loading={loading} /> : <LandingPage />}
+			<LandingPage>
+				<AllContent />
+			</LandingPage>
 		</div>
 	);
 }
