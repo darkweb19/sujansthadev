9a4f4dcf034a069e51d28251c15994081b348e53
WooFoo <sujanstha3220@gmail.com>
2023-11-26 21:20:11 +0545

    next  js landing page and links refactored

---
+++ app/notes/docs/next/Links.ts
@@ -0,0 +1,10 @@
+export const nextLinks = [
+	{
+		links: "http://localhost:3000/notes/docs/next/setup",
+		link_title: "Setting up next",
+	},
+	{
+		links: "http://localhost:3000/notes/docs/next/intro",
+		link_title: "Introduction of next",
+	},
+];
