b6d4759850eb4e30691624d7435aaa50c8a42d73
WooFoo <sujanstha3220@gmail.com>
2023-11-26 21:15:25 +0545

    removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import SocialLinks from "@/components/rightNavs/SocialLinks";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navigations from "@/components/rightNavs/Navigations";
 
 const inter = Inter({ subsets: ["latin"] });
 
