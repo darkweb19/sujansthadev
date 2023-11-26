02f801b620103f68aa0f4474be581ec66bb7d0d2
WooFoo <sujanstha3220@gmail.com>
2023-11-26 21:15:47 +0545

    added providers for nextui library

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,21 +1,44 @@
 import Navbar from "@/components/Notes/Navbar";
 import { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {
+	Lilita_One,
+	Inter,
+	Playpen_Sans,
+	Ubuntu,
+	Rubik,
+} from "next/font/google";
+import { Providers } from "../providers";
+
 export const metadata: Metadata = {
-	title: {
-		template: " Notes",
-		default: "Coding Notes",
-	},
+	title: "Coding Notes",
 	description: "A passionate full-stack web developer from Nepal.",
 	metadataBase: new URL("https://sujansthadev.com.np"),
 };
+
+export const rubik = Rubik({ subsets: ["latin"] });
+
+export const lilitaFont = Lilita_One({
+	weight: "400",
+	subsets: ["latin"],
+});
+
+export const playpenSans = Playpen_Sans({ subsets: ["latin"] });
+
+export const ubuntu = Ubuntu({
+	subsets: ["latin"],
+	weight: "700",
+});
+
 const inter = Inter({ subsets: ["latin"] });
+
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<Navbar />
-				{children}{" "}
+				<Providers>
+					<Navbar />
+					{children}{" "}
+				</Providers>
 			</body>
 		</html>
 	);
