52020f3a14f1ebea8b6284a114a0bf427d1a6c2d
WooFoo <sujanstha3220@gmail.com>
2023-11-26 21:14:31 +0545

    installed nextui library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
@@ -17,8 +19,17 @@ const config: Config = {
 				"4xl": "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
 				"5xl": "rgba(0, 0, 0, 0.45) -1rem 15px 20px -20px",
 			},
+			colors: {
+				custom: "#EEE4E1",
+				"slate-black": "#333333",
+				"slate-white": "#F2F2F2",
+				"light-white": "#EEEEEE",
+				"dark-green": "#6B654C",
+				"light-green": "#8B8779",
+			},
 		},
 	},
-	plugins: [],
+	darkMode: "class",
+	plugins: [nextui()],
 };
 export default config;
