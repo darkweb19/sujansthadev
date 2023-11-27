6b8a26a9d253c7412b2e415cfda7579881a8b998
WooFoo <sujanstha3220@gmail.com>
2023-11-27 19:20:05 +0545

    meta data changed of both intro and setup

--- app/notes/(docs)/react/intro/page.tsx
+++ app/notes/(docs)/react/intro/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		title: "React Js | Introduction ",
+		description: "Notes of React js including some snippets",
+	};
+}
 export default function intoReact() {
 	return <div>Introduction to react</div>;
 }
--- app/notes/(docs)/react/setup/page.tsx
+++ app/notes/(docs)/react/setup/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		title: "How to Set Up React | React JS",
+		description: "Notes of React js including some snippets",
+	};
+}
 export default function SetUp() {
 	return <div>This is setup for react</div>;
 }
