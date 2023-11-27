67693f54899afd7af49231b5a0c9ba5383559086
WooFoo <sujanstha3220@gmail.com>
2023-11-27 17:10:51 +0545

    next links and react links are created

---
+++ app/notes/(docs)/react/Links.ts
@@ -0,0 +1,10 @@
+export const reactLinks = [
+	{
+		links: "/notes/react/setup",
+		link_title: "Setting up react",
+	},
+	{
+		links: "/notes/react/intro",
+		link_title: "Introduction of React",
+	},
+];
