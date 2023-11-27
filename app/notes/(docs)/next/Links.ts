c3f1e4dfdfddaa7d925bcb3e2e83875ad06f15e5
WooFoo <sujanstha3220@gmail.com>
2023-11-27 19:19:21 +0545

    added mainPage link to react and next

--- app/notes/(docs)/next/Links.ts
+++ app/notes/(docs)/next/Links.ts
@@ -1,10 +1,14 @@
 export const nextLinks = [
 	{
-		links: "http://localhost:3000/notes/docs/next/setup",
+		links: "/notes/next/setup",
 		link_title: "Setting up next",
 	},
 	{
-		links: "http://localhost:3000/notes/docs/next/intro",
+		links: "/notes/next/intro",
 		link_title: "Introduction of next",
 	},
 ];
+
+export const mainPageLink = {
+	link: "/notes/next",
+};
--- app/notes/(docs)/react/Links.ts
+++ app/notes/(docs)/react/Links.ts
@@ -8,3 +8,7 @@ export const reactLinks = [
 		link_title: "Introduction of React",
 	},
 ];
+
+export const mainPageLink = {
+	link: "/notes/react",
+};
