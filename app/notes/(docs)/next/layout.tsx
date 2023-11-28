137dfb35602a0e81f50d743232046a5d883261fe
WooFoo <sujanstha3220@gmail.com>
2023-11-28 11:58:35 +0545

    added arrow to aside navigation

--- app/notes/(docs)/next/layout.tsx
+++ app/notes/(docs)/next/layout.tsx
@@ -4,6 +4,7 @@ import { lato } from "../../layout";
 import { nextLinks, mainPageLink } from "./Links";
 import { usePathname } from "next/navigation";
 import { ScrollShadow } from "@nextui-org/scroll-shadow";
+import { IoMdArrowDropright } from "react-icons/io";
 
 export default function NextLayout({
 	children,
@@ -11,23 +12,36 @@ export default function NextLayout({
 	children: React.ReactNode;
 }) {
 	const pathname = usePathname();
-	console.log(pathname);
 	return (
 		<section className={`${lato.className} font-medium h-[90vh]`}>
 			<div className=" w-full h-full flex border  ">
-				<div className="h-full w-1/5 border-r-2 border-slate-black p-2 overflow-scroll scroll-smooth">
+				<div className="h-full w-1/5 border-r-2 border-slate-black py-2 overflow-scroll scroll-smooth hidden md:block">
 					<ScrollShadow className="w-full h-full">
-						<h1 className="text-xl font-medium">
-							<Link href={mainPageLink.link}>Introduction</Link>
+						<h1 className="text-2xl text-start p-2 font-medium">
+							<Link
+								className={`${
+									pathname === mainPageLink.link &&
+									"underline underline-offset-2 duration-200 text-slate-600"
+								}`}
+								href={mainPageLink.link}
+							>
+								Introduction
+							</Link>
 						</h1>
-						<ul className=" w-full  px-3">
+						<ul className=" w-full px-1 ">
 							{nextLinks.map((item, ind) => (
-								<li key={ind}>
+								<li
+									key={ind}
+									className="px-2 flex items-center justify-start"
+								>
+									<span className=" h-fit w-fit text-2xl">
+										<IoMdArrowDropright />
+									</span>
 									<Link
 										className={`${
 											pathname === item.links &&
-											"underline underline-offset-2 duration-200"
-										} `}
+											"underline underline-offset-2 duration-200 text-slate-600"
+										} text-lg `}
 										href={item.links}
 									>
 										{" "}
@@ -37,12 +51,12 @@ export default function NextLayout({
 							))}
 							<div className="border h-80"></div>
 							<div className="border h-80"></div>
+							<div className="border h-80"></div>
 						</ul>
 					</ScrollShadow>
 				</div>
-				<div className="border-r-2 border-slate-black p-2  w-4/5 h-[90vh] overflow-scroll scroll-smooth">
+				<div className="border-r-2 border-slate-black p-2 w-full md:w-4/5 h-[90vh] overflow-scroll scroll-smooth">
 					{children}
-					<div className="border border-black h-96"></div>
 				</div>
 			</div>
 			<div></div>
--- app/notes/(docs)/react/layout.tsx
+++ app/notes/(docs)/react/layout.tsx
@@ -4,6 +4,7 @@ import { lato } from "../../layout";
 import { reactLinks, mainPageLink } from "./Links";
 import { usePathname } from "next/navigation";
 import { ScrollShadow } from "@nextui-org/scroll-shadow";
+import { IoMdArrowDropright } from "react-icons/io";
 
 export default function ReactLayout({
 	children,
@@ -14,9 +15,9 @@ export default function ReactLayout({
 	return (
 		<section className={`${lato.className} font-medium h-[90vh]`}>
 			<div className=" w-full h-full flex border  ">
-				<div className="h-full w-1/5 border-r-2 border-slate-black p-2 overflow-scroll scroll-smooth">
+				<div className="h-full w-1/5 border-r-2 border-slate-black py-2 overflow-scroll scroll-smooth hidden md:block">
 					<ScrollShadow className="w-full h-full">
-						<h1 className="text-xl font-medium">
+						<h1 className="text-2xl text-start p-2 font-medium">
 							<Link
 								className={`${
 									pathname === mainPageLink.link &&
@@ -27,14 +28,20 @@ export default function ReactLayout({
 								Introduction
 							</Link>
 						</h1>
-						<ul className=" w-full  px-3">
+						<ul className=" w-full px-1 ">
 							{reactLinks.map((item, ind) => (
-								<li key={ind}>
+								<li
+									key={ind}
+									className="px-2 flex items-center justify-start"
+								>
+									<span className=" h-fit w-fit text-2xl">
+										<IoMdArrowDropright />
+									</span>
 									<Link
 										className={`${
 											pathname === item.links &&
 											"underline underline-offset-2 duration-200 text-slate-600"
-										} `}
+										} text-lg `}
 										href={item.links}
 									>
 										{" "}
@@ -48,7 +55,7 @@ export default function ReactLayout({
 						</ul>
 					</ScrollShadow>
 				</div>
-				<div className="border-r-2 border-slate-black p-2  w-4/5 h-[90vh] overflow-scroll scroll-smooth">
+				<div className="border-r-2 border-slate-black p-2 w-full md:w-4/5 h-full overflow-scroll scroll-smooth">
 					{children}
 				</div>
 			</div>
