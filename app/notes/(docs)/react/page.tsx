f3b20f885c126e133ce819395d4290dc234b4d00
WooFoo <sujanstha3220@gmail.com>
2023-12-13 18:35:59 +0545

    changed some milisecond

--- app/notes/(docs)/react/page.tsx
+++ app/notes/(docs)/react/page.tsx
@@ -18,7 +18,7 @@ import TopNavButtons from "@/components/Notes/TopNavButtons";
 // }
 
 export default async function ReactPage() {
-	await new Promise((resolve) => setTimeout(resolve, 1000));
+	await new Promise((resolve) => setTimeout(resolve, 500));
 
 	return (
 		<div className="flex flex-col gap-1">
