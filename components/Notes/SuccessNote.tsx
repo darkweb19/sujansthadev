abb42233189a05f3133573e9d3bcd8807813397d
WooFoo <sujanstha3220@gmail.com>
2023-12-15 19:18:58 +0545

    Feat : Success Note made more dynamic

--- components/Notes/SuccessNote.tsx
+++ components/Notes/SuccessNote.tsx
@@ -1,14 +1,37 @@
 import { inter } from "@/app/notes/layout";
+import Link from "next/link";
+import React from "react";
 import { RiVerifiedBadgeFill } from "react-icons/ri";
 
-export default function SuccessNote({ content }: { content: string }) {
+interface SuccessNoteProps {
+	type: "Note" | "Tip";
+	content: string;
+	link?: string;
+	linktext?: string;
+}
+
+export default function SuccessNote({
+	type,
+	content,
+	link,
+	linktext,
+}: SuccessNoteProps) {
 	return (
 		<div className="bg-[#D7FFEB] flex gap-1 mt-3 text-justify p-3 h-fit text-[#0D924E] rounded-lg border border-[#43E790]">
 			<div className="h-fit py-1 text-xl w-fit">
 				<RiVerifiedBadgeFill />
 			</div>
 			<p className={`${inter.className} `}>
-				<span className="font-bold">Tip</span> : {content}
+				<span className="font-bold">{type}</span> : {content}{" "}
+				{link && (
+					<Link
+						className="text-blue-600 text-base font-bold"
+						href={link}
+						target="_blank"
+					>
+						({linktext}).
+					</Link>
+				)}
 			</p>
 		</div>
 	);
