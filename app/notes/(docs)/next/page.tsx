70ae098bc095efb8bb85bd33ce5b0bd780f01eeb
WooFoo <sujanstha3220@gmail.com>
2023-12-15 16:44:15 +0545

    Style :Basic Definitions of Next JS is added.

--- app/notes/(docs)/next/page.tsx
+++ app/notes/(docs)/next/page.tsx
@@ -1,3 +1,5 @@
+import MainContent from "@/components/Notes/MainContent";
+import TopNavButtons from "@/components/Notes/TopNavButtons";
 import type { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -6,6 +8,28 @@ export async function generateMetadata(): Promise<Metadata> {
 		description: "Notes of Next js including some snippets",
 	};
 }
+
 export default function NextPage() {
-	return <div>Hello From Next Page</div>;
+	return (
+		<div className="flex flex-col gap-1">
+			<TopNavButtons
+				nextLink="/notes/react/useRef"
+				prevLink="/notes/react/useState"
+			/>
+			<MainContent
+				heading={"NEXT JS"}
+				contentHeading={"What is Next JS?"}
+				mainDefinition={
+					"Next Js is an open-source react framework for web. It's designed to build react application with server-side rendering and other advanced feature."
+				}
+			>
+				<h1></h1>
+			</MainContent>
+
+			<TopNavButtons
+				nextLink="/notes/react/useRef"
+				prevLink="/notes/react/useState"
+			/>
+		</div>
+	);
 }
