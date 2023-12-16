d736cb0188710b9e65bbf190ef44db6d98a6ecb8
WooFoo <sujanstha3220@gmail.com>
2023-12-16 14:31:37 +0545

    Feat : Documenting setup for Next is completed.

---
+++ app/notes/(docs)/next/setup/DevelopmentServer.tsx
@@ -0,0 +1,38 @@
+import { inter } from "@/app/notes/layout";
+import { Code } from "@nextui-org/react";
+
+export default function DevelopmentList() {
+	return (
+		<ul
+			className={`${inter.className} list-decimal py-1 pl-5 sm:pl-8 mt-1 flex flex-col gap-2`}
+		>
+			<li>
+				{" "}
+				Run{" "}
+				<Code className="text-slate-white bg-slate-black">
+					npm run dev{" "}
+				</Code>{" "}
+				to start the development server.
+			</li>
+			<li>
+				Visit{" "}
+				<Code className="text-slate-white bg-slate-black">
+					http://localhost:3000{" "}
+				</Code>{" "}
+				to view your application.
+			</li>
+			<li>
+				Edit{" "}
+				<Code className="text-slate-white bg-slate-black">
+					app/page.tsx
+				</Code>{" "}
+				(or{"  "}
+				<Code className="text-slate-white bg-slate-black">
+					{" "}
+					pages/index.tsx
+				</Code>
+				) file and save it to see the updated result in your browser.
+			</li>
+		</ul>
+	);
+}
--- app/notes/(docs)/next/setup/page.tsx
+++ app/notes/(docs)/next/setup/page.tsx
@@ -1,3 +1,62 @@
+import { ubuntu } from "@/app/notes/layout";
+import MainContent from "@/components/Notes/MainContent";
+import SuccessNote from "@/components/Notes/SuccessNote";
+import TopNavButtons from "@/components/Notes/TopNavButtons";
+import { Snippet } from "@nextui-org/snippet";
+import DevelopmentList from "./DevelopmentServer";
+import ScriptList from "./ScriptList";
+import InstallationPrompt from "./InstallationPrompt";
+
 export default function nextSetup() {
-	return <div>This is next js SetUp page</div>;
+	return (
+		<div className="flex flex-col gap-1">
+			<TopNavButtons
+				nextLink="/notes/next/structure"
+				prevLink="/notes/next/"
+			/>
+			<MainContent
+				heading={"Nextjs Installation"}
+				contentHeading={"Installation"}
+				mainDefinition={
+					"To install Next.js, you'll first need to have Node.js and npm (Node Package Manager) installed on your machine. "
+				}
+			>
+				<div className="p-0 sm:p-1 border h-fit">
+					<h1
+						className={`${ubuntu.className} text-2xl mt-2 font-medium text-slate-black`}
+					>
+						Create a Next.js App
+					</h1>
+					<div className="px-0 sm:px-2">
+						<Snippet className="bg-slate-black text-light-white">
+							<span>npx create-next-app@latest {`<dir>`}</span>
+						</Snippet>
+						<SuccessNote content="Don't forget to follow node manager's naming conventions." />
+
+						<InstallationPrompt />
+
+						<h1
+							className={`${ubuntu.className} text-2xl mt-3 font-medium text-slate-black`}
+						>
+							Scripts :
+						</h1>
+						<ScriptList />
+
+						<h1
+							className={`${ubuntu.className} text-2xl mt-4 font-medium text-slate-black`}
+						>
+							Run Development Server
+						</h1>
+
+						<DevelopmentList />
+					</div>
+				</div>
+			</MainContent>
+
+			<TopNavButtons
+				nextLink="/notes/next/structure"
+				prevLink="/notes/next/"
+			/>
+		</div>
+	);
 }
