fbaa207ecd49033769ba3cb5a3c701994f3a0ddb
WooFoo <sujanstha3220@gmail.com>
2023-12-19 21:42:53 +0545

    refac : Metadata for Data fetching in Next.

--- app/notes/(docs)/next/fetching/page.tsx
+++ app/notes/(docs)/next/fetching/page.tsx
@@ -1,6 +1,15 @@
 import MainContent from "@/components/Notes/MainContent";
 import TopNavButtons from "@/components/Notes/TopNavButtons";
 import DataFetchingAll from "./DataFetchingAll";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		title: "Data Fetching Pattern : Server Component",
+		description:
+			"How to you can fetch, cache, and revalidate data in React and Next.js. ",
+	};
+}
 
 export default function Fetching() {
 	return (
