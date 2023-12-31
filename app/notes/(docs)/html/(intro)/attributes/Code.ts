a8813e5e9015f2671a4a3aa625117b347e04caad
WooFoo <sujanstha3220@gmail.com>
2023-12-31 13:52:34 +0545

    feat : more content in attributes page

--- app/notes/(docs)/html/(intro)/attributes/Code.ts
+++ app/notes/(docs)/html/(intro)/attributes/Code.ts
@@ -2,3 +2,12 @@ export const idCode = ` <p id="html">This is an HTML tutorial</p>
   <p id="python">This is a Python tutorial</p>`;
 
 export const titleCode = `<h4 title="hello, motto">Title attribute</h4>`;
+
+export const heightCode = `<img src="img_girl.jpg" width="500" height="600">`;
+
+export const langCode = `<!DOCTYPE html>
+<html lang="en">
+<body>
+...
+</body>
+</html>`;
