2b0a2d28d890a5dd0b4e5ca25f0f0c6ee0bd2a9e
WooFoo <sujanstha3220@gmail.com>
2024-02-04 15:13:05 +0545

    feat : basic tags are documented

--- app/notes/(docs)/html/(basic tags)/heading/page.tsx
+++ app/notes/(docs)/html/(basic tags)/heading/page.tsx
@@ -1,3 +1,18 @@
+import MainContent from "@/components/Notes/MainContent";
+import HeadingTagMdx from "./HeadingTag.mdx";
+
 export default function HeadingTag() {
-	return <div>Heading tag</div>;
+	return (
+		<div className="flex flex-col gap-1">
+			<MainContent
+				heading={"HEADING TAGS"}
+				contentHeading={""}
+				mainDefinition={
+					"Heading tags in HTML are used to define headings or titles within a document. There are six levels of heading tags, ranging from `<h1> `to`<h6>`, with `<h1>` being the highest level (most important) and `<h6>` being the lowest level."
+				}
+			>
+				<HeadingTagMdx />
+			</MainContent>
+		</div>
+	);
 }
