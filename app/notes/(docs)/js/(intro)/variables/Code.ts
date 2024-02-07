5a312af6274e5aa57d2ca01705755b0afefca73d
WooFoo <sujanstha3220@gmail.com>
2024-02-07 20:43:46 +0545

    feat : js introduction documentation done

---
+++ app/notes/(docs)/js/(intro)/variables/Code.ts
@@ -0,0 +1,9 @@
+export const variableCode = `var x = 5;
+let y = "Hello";
+const z = true;`;
+
+export const dataCode = `typeof 42; // "number"
+typeof "hello"; // "string"
+typeof true; // "boolean"
+typeof null; // "object" (this is a quirk of JavaScript)
+typeof undefined; // "undefined"`;
--- app/notes/(docs)/js/(intro)/variables/page.tsx
+++ app/notes/(docs)/js/(intro)/variables/page.tsx
@@ -1,3 +1,35 @@
+import MainContent from "@/components/Notes/MainContent";
+import TopNavButtons from "@/components/Notes/TopNavButtons";
+import { RiJavascriptFill } from "react-icons/ri";
+import VariablesMdx from "./Variables.mdx";
+import SuccessNote from "@/components/Notes/SuccessNote";
+
 export default function jsVariables() {
-	return <div>hello from variables</div>;
+	return (
+		<div className="flex flex-col gap-1">
+			<TopNavButtons
+				nextLink="/notes/js/diffvar"
+				prevdisabled
+				prevLink="#"
+			/>
+
+			<MainContent
+				heading={"VARIABLES"}
+				contentHeading={"What are Variables?"}
+				mainDefinition={
+					"In JavaScript, variables are used to store data. They are an essential part of any programming language, as they allow you to store, retrieve, and manipulate data in your programs."
+				}
+				logo={<RiJavascriptFill />}
+			>
+				<VariablesMdx />
+				<SuccessNote content="JavaScript is dynamically typed language ,so type checking takes place at runtime or execution time." />
+			</MainContent>
+
+			<TopNavButtons
+				nextLink="/notes/js/diffvar"
+				prevdisabled
+				prevLink="#"
+			/>
+		</div>
+	);
 }
