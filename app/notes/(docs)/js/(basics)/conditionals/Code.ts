c9d4fab8fd71cdebe0ab2b87be8b257aeb7ed246
WooFoo <sujanstha3220@gmail.com>
2024-02-08 20:29:44 +0545

    feat : written js code for conditionals

---
+++ app/notes/(docs)/js/(basics)/conditionals/Code.ts
@@ -0,0 +1,8 @@
+export const conditionalsCode = `let x = 5;
+if (x > 10) {
+    console.log("x is greater than 10");
+} else if (x < 10) {
+    console.log("x is less than 10");
+} else {
+    console.log("x is equal to 10");
+}`;
