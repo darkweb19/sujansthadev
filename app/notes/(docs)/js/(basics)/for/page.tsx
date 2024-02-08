991791ed042a2e9d22714694ed740873a8f44e66
WooFoo <sujanstha3220@gmail.com>
2024-02-08 20:30:02 +0545

    feat : js notes for for loop

--- app/notes/(docs)/js/(basics)/for/page.tsx
+++ app/notes/(docs)/js/(basics)/for/page.tsx
@@ -1,3 +1,31 @@
+import MainContent from "@/components/Notes/MainContent";
+import TopNavButtons from "@/components/Notes/TopNavButtons";
+import { RiJavascriptFill } from "react-icons/ri";
+import ForLoopMdx from "./ForLoop.mdx";
+
 export default function jsForLoop() {
-	return <div> Hello from for loop in js</div>;
+	return (
+		<div className="flex flex-col gap-1">
+			<TopNavButtons
+				nextLink="/notes/js/while"
+				prevLink="/notes/js/conditionals"
+			/>
+
+			<MainContent
+				heading={"LOOPS"}
+				contentHeading={"Loops in Js"}
+				mainDefinition={
+					"Loops are used to repeatedly execute a block of code until a certain condition is met."
+				}
+				logo={<RiJavascriptFill />}
+			>
+				<ForLoopMdx />
+			</MainContent>
+
+			<TopNavButtons
+				nextLink="/notes/js/while"
+				prevLink="/notes/js/conditionals"
+			/>
+		</div>
+	);
 }
