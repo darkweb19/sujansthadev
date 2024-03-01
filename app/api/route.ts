b86b6f618bc34760fbb5b9549af3b595010afded
WooFoo <sujanstha3220@gmail.com>
2024-03-01 16:16:16 +0545

    feat : routes are created for user and testing

---
+++ app/api/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/utils/Prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+	try {
+		// await prisma.user.create({
+		// 	data: {
+		// 		name: "Sujan Shrestha",
+		// 		email: "sujansthadev@gmail.com",
+		// 		messages: {},
+		// 	},
+		// });
+
+		// await prisma.message.create({
+		// 	data: {
+		// 		message: "Hello sujan , can you work for me ,",
+		// 		authorId: "eda8dadd-99ca-4e43-894b-5893b7afc0a9",
+		// 	},
+		// });
+		return NextResponse.json({ message: "Created" });
+	} catch (err: any) {
+		console.log(err.message);
+		return NextResponse.json({ message: "Err : ", error: err.message });
+	}
+}
